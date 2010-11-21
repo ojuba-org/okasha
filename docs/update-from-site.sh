d1a27e77de25a129bde51790519ceba27368e7e4
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2010-11-21 16:15:15 +0200

    updated documents and release

--- docs/update-from-site.sh
+++ docs/update-from-site.sh
@@ -9,18 +9,25 @@ rm "$fn" 2>/dev/null || :
 curl -L -o "$fn" "${baseurl}${i}"
 
 perl -i -lwne 'BEGIN{$echo=1;}
+s:okasha-logo:logo:g;
 s:href="/wiki/okasha/([^"]+)":href="${1}.html":g;
 s:src="/wiki/_media/okasha/([^?"]+)(\?[^"]*)?":src="../files/$1":g;
 s:href="/wiki/_detail/okasha/([^?"]+)(\?[^"]*)?":href="../files/$1":g;
-s:okasha-logo:logo:g;
+s!a href="http://!a target="_blank" href="http://!g;
+if(/\<head[^>]*\>/){$echo=0;}
 if(/#discussion__section|\<(link|meta|script)[^>]*\>/){next;}if (/class="tags"/) {$echo=0;}
+if($echo){print $_;}if (/\<\/div\>/) {$echo=1;}
 if(/\<\/head\>/) {
+ print "<head>";
  print "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
+ print "<title>وثائق عكاشة</title>";
  print "<link rel=\"stylesheet\" media=\"all\" type=\"text/css\" href=\"all.css\" />";
  print "<link rel=\"stylesheet\" media=\"screen\" type=\"text/css\" href=\"screen.css\" />";
  print "<link rel=\"stylesheet\" media=\"print\" type=\"text/css\" href=\"print.css\" />";
+ print "</head>";
+ $echo=1;
 }
-if($echo){print $_;}if (/\<\/div\>/) {$echo=1;}' "$fn"
+' "$fn"
 
 done
 
